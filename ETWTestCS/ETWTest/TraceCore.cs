285799f74ffc30d0ced6eb6cf0bee735878d6e89
idrilsilverfoot <chew.esmero@gmail.com>
2016-03-19 23:21:36 +0900

    Add a sample C# app to test ETW in CS.
    Also added a gitignore file for the sample.

---
+++ ETWTestCS/ETWTest/TraceCore.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETWTest
+{
+    public static class TraceCore
+    {
+        public static bool Verbose(
+            string m,
+            [CallerMemberName] string memberName = "?",
+            [CallerFilePath] string srcFile = "?",
+            [CallerLineNumber] int srcNum = 0)
+        {
+            return JyTrace.ProviderJyTrace.EventWriteSimple("ETWTest", Path.GetFileName(srcFile), memberName, "Trace:", m);
+        }
+    }
+}
