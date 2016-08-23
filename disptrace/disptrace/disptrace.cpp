242a7742f8e4242c596dc3a4f54cae6db930aead
Chew Esmero <chew.esmero@gmail.com>
2016-08-24 01:07:19 +0900

    Exportable etw function.

--- disptrace/disptrace/disptrace.cpp
+++ disptrace/disptrace/disptrace.cpp
@@ -4,7 +4,7 @@
 #include "stdafx.h"
 #include "..\..\jytrace.h"
 
-_declspec(dllexport) void __stdcall ETWTrace(wchar_t *szMod, wchar_t *szFile, wchar_t *szFn, wchar_t *szKey, wchar_t *szVal)
+void __stdcall ETWTrace(wchar_t *szMod, wchar_t *szFile, wchar_t *szFn, wchar_t *szKey, wchar_t *szVal)
 {
     EventWriteSimple(szMod, szFile, szFn, szKey, szVal);
 }
